d6a26ea23f8f90c333c697b12b2cb686f54bba75
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2021-12-21 18:55:21 +1100

    boiler plate

---
+++ boiler_plate.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data) {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* build_tree() {
+    int input;
+    cin >> input;
+
+    if (input == -1) return NULL;
+
+    Node* node = new Node(input);
+    node->left = build_tree();
+    node->right = build_tree();
+
+    return node;
+}
+
+int main(void) {
+    Node* root = build_tree();
+
+    return 0;
+}
