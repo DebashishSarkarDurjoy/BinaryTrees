c41e26ecd09faca62cdb70fa56c9e4e212eb42db
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2022-02-14 07:10:43 +1100

    level order print practice 2

---
+++ bt_boiler_p.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int i) {
+        this->data = i;
+        this->left = NULL;
+        this->right = NULL;
+    }
+
+};
+
+Node* buildTree() {
+
+    int i;
+    cin >> i;
+
+    if(i == -1) return NULL;
+
+    Node* node = new Node(i);
+    node->left = buildTree();
+    node->right = buildTree();
+
+    return node;
+}
+
+void levelOrderPrint(Node* root) {
+    queue<Node*> q;
+
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty()) {
+        Node* f = q.front();
+        q.pop();
+
+        if (f == NULL) {
+            cout << endl;
+            if (!q.empty()) {
+                q.push(NULL);
+                continue;
+            }
+        }
+
+        cout << f->data << " ";
+        if (f->left != NULL) {
+            q.push(f->left);
+        }
+        if (f->right != NULL) {
+            q.push(f->right);
+        }
+
+    }
+}
+
+
+int main(void) {
+
+    Node* root = buildTree();
+
+    levelOrderPrint2(root);
+
+    return 0;
+}
