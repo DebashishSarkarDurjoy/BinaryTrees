6a3edaf30d286cfef5157349d1a97f5837830b79
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2022-11-08 20:43:11 +1100

    binary tree boiler plate practice 7

---
+++ binaryTree7.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class leaf {
+public:
+    int data;
+    leaf* left;
+    leaf* right;
+
+    leaf(int i) {
+        this->data = i;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+leaf* buildTree() {
+    int input;
+    cin >> input;
+    if (input == -1) return NULL;
+
+    leaf* node = new leaf(input);
+    node->left = buildTree();
+    node->right = buildTree();
+
+    return node;
+}
+
+int main(void) {
+    leaf* head = buildTree();
+
+
+    return 0;
+}
